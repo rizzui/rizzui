0ec3b3997f2b8a85aa959a0b99b68b624adf92ee
Md Nahid <nahid98learn@gmail.com>
2023-04-17 13:18:05 +0600

    some component transfered

--- apps/docs/src/components/native-select.tsx
+++ apps/docs/src/components/native-select.tsx
@@ -1,139 +1,67 @@
-import React, { useState } from 'react';
-import { NativeSelect } from '@redq/rizz';
+import React, { useState } from "react";
+import { NativeSelect } from "@redq/rizz";
 
 const options = [
-	{
-		id: 1,
-		name: 'John',
-		value: 'John',
-	},
-	{
-		age: 20,
-		id: 2,
-		name: 'Doe',
-		value: 'Doe',
-	},
-	{
-		class: 12,
-		id: 3,
-		name: 'Miller',
-		value: 'Miller',
-	},
-	{
-		id: 4,
-		name: 'James',
-		value: 'James',
-	},
-	{
-		disabled: true,
-		id: 5,
-		name: 'Franky',
-		value: 'Franky',
-	},
+  {
+    id: 1,
+    name: "John",
+    value: "John",
+  },
+  {
+    age: 20,
+    id: 2,
+    name: "Doe",
+    value: "Doe",
+  },
+  {
+    class: 12,
+    id: 3,
+    name: "Miller",
+    value: "Miller",
+  },
+  {
+    id: 4,
+    name: "James",
+    value: "James",
+  },
+  {
+    disabled: true,
+    id: 5,
+    name: "Franky",
+    value: "Franky",
+  },
 ];
 
-export default function NativeSelectDefault() {
-	const [value, setValue] = useState([]);
+export default function NativeSelectDefault({
+  variant,
+  label,
+  size,
+  rounded,
+  color,
+  disabled,
+  clearable,
+  dropDownIcon,
+  helperText,
+  error,
+}) {
+  const [value, setValue] = useState([]);
 
-	return (
-		<NativeSelect
-			label="Select Name"
-			options={options}
-			placeholder="Choose option from below"
-			selectedValue={value}
-			setSelectedValue={setValue}
-		/>
-	);
-}
-
-export function NativeSelectCustomIcon() {
-	const [value, setValue] = useState([]);
-
-	return (
-		<NativeSelect
-			label="Select Name"
-			placeholder="Choose option from below"
-			options={options}
-			selectedValue={value}
-			setSelectedValue={setValue}
-			dropDownIcon={
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={1.5}
-					stroke="currentColor"
-					className="mr-2 h-5 w-5"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-					/>
-				</svg>
-			}
-		/>
-	);
-}
-
-export function NativeSelectHelperText() {
-	const [value, setValue] = useState([]);
-
-	return (
-		<NativeSelect
-			label="Select Name"
-			placeholder="Choose option from below"
-			options={options}
-			selectedValue={value}
-			setSelectedValue={setValue}
-			dropDownIcon={
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={1.5}
-					stroke="currentColor"
-					className="mr-2 h-5 w-5"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-					/>
-				</svg>
-			}
-			helperText="Please enter your full name"
-		/>
-	);
-}
-
-export function NativeSelectError() {
-	const [value, setValue] = useState([]);
-
-	return (
-		<NativeSelect
-			label="Select Name"
-			placeholder="Choose option from below"
-			options={options}
-			selectedValue={value}
-			setSelectedValue={setValue}
-			dropDownIcon={
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={1.5}
-					stroke="currentColor"
-					className="mr-2 h-5 w-5"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-					/>
-				</svg>
-			}
-			error="This field is required"
-		/>
-	);
+  return (
+    <NativeSelect
+      size={size}
+      label={label}
+      variant={variant}
+      options={options}
+      rounded={rounded}
+      selectedValue={value}
+      setSelectedValue={setValue}
+      placeholder="Choose option from below"
+      color={color}
+      disabled={disabled}
+      clearable={clearable}
+      dropDownIcon={dropDownIcon}
+      helperText={helperText}
+      error={error}
+    />
+  );
 }
