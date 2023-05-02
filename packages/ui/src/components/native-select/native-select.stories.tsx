42b06bf07aa1a405ed38cda58abbce8b526a44a0
Idris <idrisali725@gmail.com>
2023-05-02 20:35:28 +0600

    NativeSelect updated

--- packages/ui/src/components/native-select/native-select.stories.tsx
+++ packages/ui/src/components/native-select/native-select.stories.tsx
@@ -1,38 +1,9 @@
 import * as React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import NativeSelect, { OptionsType } from '.';
+import NativeSelect from './native-select';
 
-const data = [
-  {
-    id: 1,
-    name: 'John',
-    value: 'John',
-  },
-  {
-    id: 2,
-    name: 'Doe',
-    value: 'Doe',
-    age: 20,
-  },
-  {
-    id: 3,
-    name: 'Miller',
-    value: 'Miller',
-    class: 12,
-  },
-  {
-    id: 4,
-    name: 'James',
-    value: 'James',
-  },
-  {
-    id: 5,
-    name: 'Franky',
-    value: 'Franky',
-    disabled: true,
-  },
-];
+const options = ['John', 'Miller', 'James', 'Franky'];
 
 export default {
   title: 'Components/NativeSelect',
@@ -43,194 +14,74 @@ export default {
 } as ComponentMeta<typeof NativeSelect>;
 
 const Template: ComponentStory<typeof NativeSelect> = (args) => {
-  const [value, setValue] = React.useState<OptionsType[]>([]);
-  return (
-    <NativeSelect {...args} selectedValue={value} setSelectedValue={setValue} />
-  );
+  return <NativeSelect {...args} options={options} />;
 };
 
 export const Default = Template.bind({});
 Default.args = {
-  options: data,
   label: 'Select Name',
-  placeholder: 'Choose option from below',
 };
 
-export const WithCustomDropdownIcon = () => {
+export const Disabled = Template.bind({});
+Disabled.args = {
+  label: 'Select Name',
+  disabled: true,
+};
+
+export const WithDisableOption = () => {
   const options = [
     {
       id: 1,
-      name: 'John',
+      label: 'John',
       value: 'John',
     },
     {
       id: 2,
-      name: 'Doe',
+      label: 'Doe',
       value: 'Doe',
-      age: 20,
     },
     {
       id: 3,
-      name: 'Miller',
+      label: 'Miller',
       value: 'Miller',
-      class: 12,
     },
     {
       id: 4,
-      name: 'James',
+      label: 'James',
       value: 'James',
     },
     {
       id: 5,
-      name: 'Franky',
+      label: 'Franky',
       value: 'Franky',
       disabled: true,
     },
   ];
-  const [value, setValue] = React.useState<OptionsType[]>([]);
+  return <NativeSelect label="Store name" options={options} />;
+};
+
+export const WithClearButton = () => {
+  const [state, setState] = React.useState('John');
   return (
     <NativeSelect
-      label="Select Name"
-      placeholder="Choose option from below"
-      options={options}
-      selectedValue={value}
-      setSelectedValue={setValue}
-      dropDownIcon={
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="mr-2 h-5 w-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-          />
-        </svg>
-      }
+      label="Store name"
+      value={state}
+      options={['John', 'Miller', 'James', 'Franky']}
+      onChange={(e) => setState(e.target.value)}
+      clearable={!!state}
+      onClear={() => setState('')}
     />
   );
 };
 
-export const WithHelperText = () => {
-  const options = [
-    {
-      id: 1,
-      name: 'John',
-      value: 'John',
-    },
-    {
-      id: 2,
-      name: 'Doe',
-      value: 'Doe',
-      age: 20,
-    },
-    {
-      id: 3,
-      name: 'Miller',
-      value: 'Miller',
-      class: 12,
-    },
-    {
-      id: 4,
-      name: 'James',
-      value: 'James',
-    },
-    {
-      id: 5,
-      name: 'Franky',
-      value: 'Franky',
-      disabled: true,
-    },
-  ];
-  const [value, setValue] = React.useState<OptionsType[]>([]);
-  return (
-    <NativeSelect
-      label="Select Name"
-      placeholder="Choose option from below"
-      options={options}
-      selectedValue={value}
-      setSelectedValue={setValue}
-      dropDownIcon={
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="mr-2 h-5 w-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-          />
-        </svg>
-      }
-      helperText="Please enter your full name"
-    />
-  );
+export const WithHelperText = Template.bind({});
+WithHelperText.args = {
+  label: 'Select Name',
+  helperText: 'Please choose your name',
 };
 
-export const WithErrorMessage = () => {
-  const options = [
-    {
-      id: 1,
-      name: 'John',
-      value: 'John',
-    },
-    {
-      id: 2,
-      name: 'Doe',
-      value: 'Doe',
-      age: 20,
-    },
-    {
-      id: 3,
-      name: 'Miller',
-      value: 'Miller',
-      class: 12,
-    },
-    {
-      id: 4,
-      name: 'James',
-      value: 'James',
-    },
-    {
-      id: 5,
-      name: 'Franky',
-      value: 'Franky',
-      disabled: true,
-    },
-  ];
-  const [value, setValue] = React.useState<OptionsType[]>([]);
-  return (
-    <NativeSelect
-      label="Select Name"
-      placeholder="Choose option from below"
-      options={options}
-      selectedValue={value}
-      setSelectedValue={setValue}
-      dropDownIcon={
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="mr-2 h-5 w-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-          />
-        </svg>
-      }
-      error="This field is required"
-    />
-  );
+export const WithValidationError = Template.bind({});
+WithValidationError.args = {
+  label: 'Select Name',
+  error: 'This field is required',
 };
