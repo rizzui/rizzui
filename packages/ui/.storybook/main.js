f24d3611a78110452f3f325a547e1fb1f7b045d1
idris95 <idrisali725@gmail.com>
2023-10-06 09:18:23 +0600

    fix storybook tailwindcss issue

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -17,6 +17,34 @@ module.exports = {
         postCss: true,
       },
     },
+    '@storybook/addon-styling-webpack',
+    {
+      name: '@storybook/addon-styling-webpack',
+
+      options: {
+        rules: [
+          {
+            test: /\.css$/,
+            sideEffects: true,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  implementation: require.resolve('postcss'),
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
   ],
   // generate extended prop args table
   typescript: {
