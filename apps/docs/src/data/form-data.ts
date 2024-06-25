c5298a1d2e8816d79087062fbb44a54840919a4f
Md Nahid <nahid98learn@gmail.com>
2024-06-25 19:18:38 +0600

    form validation added

---
+++ apps/docs/src/data/form-data.ts
@@ -0,0 +1,170 @@
+export const genderOptions = [
+  { label: "Male", value: "male" },
+  { label: "Female", value: "female" },
+  { label: "Others", value: "others" },
+];
+
+export const skillOptions = [
+  {
+    label: "React",
+    value: "react",
+  },
+  {
+    label: "Vue",
+    value: "vue",
+  },
+  {
+    label: "Angular",
+    value: "angular",
+  },
+  {
+    label: "Svelte",
+    value: "svelte",
+  },
+  {
+    label: "Ember",
+    value: "ember",
+  },
+  {
+    label: "Backbone",
+    value: "backbone",
+  },
+  {
+    label: "JQuery",
+    value: "jquery",
+  },
+  {
+    label: "Vanilla JS",
+    value: "vanillaJS",
+  },
+  {
+    label: "Node JS",
+    value: "nodeJS",
+  },
+  {
+    label: "Deno",
+    value: "deno",
+  },
+  {
+    label: "Python",
+    value: "python",
+  },
+  {
+    label: "Ruby",
+    value: "ruby",
+  },
+  {
+    label: "Java",
+    value: "java",
+  },
+  {
+    label: "Kotlin",
+    value: "kotlin",
+  },
+  {
+    label: "Swift",
+    value: "swift",
+  },
+  {
+    label: "Dart",
+    value: "dart",
+  },
+  {
+    label: "Flutter",
+    value: "flutter",
+  },
+  {
+    label: "React Native",
+    value: "reactNative",
+  },
+  {
+    label: "Ionic",
+    value: "ionic",
+  },
+  {
+    label: "Cordova",
+    value: "cordova",
+  },
+  {
+    label: "PhoneGap",
+    value: "phoneGap",
+  },
+  {
+    label: "Xamarin",
+    value: "xamarin",
+  },
+  {
+    label: "NativeScript",
+    value: "nativeScript",
+  },
+  {
+    label: "Expo",
+    value: "expo",
+  },
+  {
+    label: "Next JS",
+    value: "nextJS",
+  },
+  {
+    label: "Gatsby",
+    value: "gatsby",
+  },
+  {
+    label: "Nuxt",
+    value: "nuxt",
+  },
+  {
+    label: "Sapper",
+    value: "sapper",
+  },
+  {
+    label: "Blitz",
+    value: "blitz",
+  },
+  {
+    label: "Redwood",
+    value: "redwood",
+  },
+  {
+    label: "Meteor",
+    value: "meteor",
+  },
+  {
+    label: "SvelteKit",
+    value: "svelteKit",
+  },
+  {
+    label: "Angular Universal",
+    value: "angularUniversal",
+  },
+  {
+    label: "Nest JS",
+    value: "nestJS",
+  },
+  {
+    label: "Express",
+    value: "express",
+  },
+  {
+    label: "Koa",
+    value: "koa",
+  },
+];
+
+export const paymentOptions = [
+  {
+    value: "single",
+    title: "Single Payment",
+    description: "Charge a one-time fee",
+  },
+  {
+    value: "subscription",
+    title: "Subscription",
+    description: "Charge an ongoing fee",
+  },
+  {
+    value: "leadMagnet",
+    title: "Lead Magnet",
+    description: "Allow access for free",
+  },
+];
--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   content: [
     "./docs/**/*.{md,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/**/*.{js,ts,jsx,tsx,mdx}",
     "./docusaurus.config.js",
     "./node_modules/rizzui/dist/*.{js,ts,jsx,tsx}",
   ],
