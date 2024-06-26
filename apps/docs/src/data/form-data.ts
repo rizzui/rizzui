ba1a3607050571624a167f8916d78c3e510832af
Md Nahid <nahid98learn@gmail.com>
2024-06-26 09:07:52 +0600

    v0.8.5

--- apps/docs/src/data/form-data.ts
+++ apps/docs/src/data/form-data.ts
@@ -1,170 +1,34 @@
-export const genderOptions = [
-  { label: "Male", value: "male" },
-  { label: "Female", value: "female" },
-  { label: "Others", value: "others" },
-];
-
-export const skillOptions = [
+export const companyOptions = [
   {
-    label: "React",
-    value: "react",
+    label: "Google Inc",
+    value: "google",
   },
   {
-    label: "Vue",
-    value: "vue",
+    label: "RizzUI Inc",
+    value: "rizzui",
   },
   {
-    label: "Angular",
-    value: "angular",
+    label: "Microsoft Inc",
+    value: "microsoft",
   },
   {
-    label: "Svelte",
-    value: "svelte",
+    label: "Meta Inc",
+    value: "meta",
   },
   {
-    label: "Ember",
-    value: "ember",
+    label: "Amazon Inc",
+    value: "amazon",
   },
   {
-    label: "Backbone",
-    value: "backbone",
+    label: "Tesla Inc",
+    value: "tesla",
   },
   {
-    label: "JQuery",
-    value: "jquery",
+    label: "Envato Inc",
+    value: "envato",
   },
   {
-    label: "Vanilla JS",
-    value: "vanillaJS",
-  },
-  {
-    label: "Node JS",
-    value: "nodeJS",
-  },
-  {
-    label: "Deno",
-    value: "deno",
-  },
-  {
-    label: "Python",
-    value: "python",
-  },
-  {
-    label: "Ruby",
-    value: "ruby",
-  },
-  {
-    label: "Java",
-    value: "java",
-  },
-  {
-    label: "Kotlin",
-    value: "kotlin",
-  },
-  {
-    label: "Swift",
-    value: "swift",
-  },
-  {
-    label: "Dart",
-    value: "dart",
-  },
-  {
-    label: "Flutter",
-    value: "flutter",
-  },
-  {
-    label: "React Native",
-    value: "reactNative",
-  },
-  {
-    label: "Ionic",
-    value: "ionic",
-  },
-  {
-    label: "Cordova",
-    value: "cordova",
-  },
-  {
-    label: "PhoneGap",
-    value: "phoneGap",
-  },
-  {
-    label: "Xamarin",
-    value: "xamarin",
-  },
-  {
-    label: "NativeScript",
-    value: "nativeScript",
-  },
-  {
-    label: "Expo",
-    value: "expo",
-  },
-  {
-    label: "Next JS",
-    value: "nextJS",
-  },
-  {
-    label: "Gatsby",
-    value: "gatsby",
-  },
-  {
-    label: "Nuxt",
-    value: "nuxt",
-  },
-  {
-    label: "Sapper",
-    value: "sapper",
-  },
-  {
-    label: "Blitz",
-    value: "blitz",
-  },
-  {
-    label: "Redwood",
-    value: "redwood",
-  },
-  {
-    label: "Meteor",
-    value: "meteor",
-  },
-  {
-    label: "SvelteKit",
-    value: "svelteKit",
-  },
-  {
-    label: "Angular Universal",
-    value: "angularUniversal",
-  },
-  {
-    label: "Nest JS",
-    value: "nestJS",
-  },
-  {
-    label: "Express",
-    value: "express",
-  },
-  {
-    label: "Koa",
-    value: "koa",
-  },
-];
-
-export const paymentOptions = [
-  {
-    value: "single",
-    title: "Single Payment",
-    description: "Charge a one-time fee",
-  },
-  {
-    value: "subscription",
-    title: "Subscription",
-    description: "Charge an ongoing fee",
-  },
-  {
-    value: "leadMagnet",
-    title: "Lead Magnet",
-    description: "Allow access for free",
+    label: "Tailwind Inc",
+    value: "tailwind",
   },
 ];
