bec09d98ae0b45797b4271e3247f99cd28ea5f86
Md Mir Idris Ali <idrisali725@gmail.com>
2024-07-16 17:16:54 +0600

    updates

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "RedQ", // Usually your GitHub org/user name.
+  organizationName: "rizzui", // Usually your GitHub org/user name.
   projectName: "rizzui", // Usually your repo name.
 
   onBrokenLinks: "throw",
