899198aab7d86aafecbe3b35261e617b6c80b196
Md Nahid <nahid98learn@gmail.com>
2024-11-04 10:01:15 +0600

    noting

--- apps/docs/src/components/alert.tsx
+++ apps/docs/src/components/alert.tsx
@@ -5,21 +5,21 @@ export default function AlertClearable() {
   const [state, setState] = React.useState(true);
   return (
     <>
-      <Button onClick={() => setState(true)} className="tracking-wider">
+      <Button
+        onClick={() => setState(true)}
+        className="tracking-wider"
+      >
         Info Alert
       </Button>
       {state && (
         <Alert
+          closable
           color="info"
           variant="flat"
-          closable
           onClose={() => setState(false)}
         >
           <p className="font-semibold">Alert with info</p>
-          <p>
-            Attention All! We are excited to announce the launch of our new
-            product/service.
-          </p>
+          <p>Attention All! We are excited to announce the launch of our new product/service.</p>
         </Alert>
       )}
     </>
